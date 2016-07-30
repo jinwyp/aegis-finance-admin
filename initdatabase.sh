e38257ee859c3adb99f8781f5a09adb22342d4bd
liuxinjie@123 <liuxinjie@yimei180.com>
2016-07-30 11:34:10 +0800

    database

--- initdatabase.sh
+++ initdatabase.sh
@@ -7,14 +7,14 @@ DATABASE="financedb"
 
 
 #crate database
-mysql -u $USER << EOF 2>/dev/null
+mysql -u $USER << EOF
 CREATE DATABASE $DATABASE
 EOF
 [ $? -eq 0 ] && echo "created DB" || echo DB already exists
 
 
 #grant right
-mysql -u $USER << EOF 2>/dev/null
+mysql -u $USER << EOF
 grant all on financedb.* to 'mysql'@127.0.0.1 identified by 'mysql'
 EOF
 [ $? -eq 0 ] && echo "grant right success" || echo "grant right failure" 
