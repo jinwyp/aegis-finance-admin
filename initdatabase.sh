4e8ef389d15b3ea72b62f60e4fde0960f572bd80
liuxinjie@123 <liuxinjie@yimei180.com>
2016-07-30 11:33:58 +0800

    database

--- initdatabase.sh
+++ initdatabase.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-##############################
+
 # create database
-##############################
+
 USER="root"
 DATABASE="financedb"
-######################
+
+
 #crate database
 mysql -u $USER << EOF 2>/dev/null
 CREATE DATABASE $DATABASE
 EOF
 [ $? -eq 0 ] && echo "created DB" || echo DB already exists
-######################
+
+
 #grant right
 mysql -u $USER << EOF 2>/dev/null
 grant all on financedb.* to 'mysql'@127.0.0.1 identified by 'mysql'
