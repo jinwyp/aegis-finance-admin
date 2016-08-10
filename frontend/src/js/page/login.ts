7f02574fda081ba547081ff54c6603753ab8f3fa
JinWYP <jinwyp@gmail.com>
2016-08-10 19:01:50 +0800

    update

--- frontend/src/js/page/login.ts
+++ frontend/src/js/page/login.ts
@@ -1,6 +1,16 @@
+import { XHRBackend } from '@angular/http';
+
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }               from '../mock/api/in-memory-data.service';
+
 
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { HTTP_PROVIDERS } from '@angular/http';
 import { LoginComponent } from '../components/login/login-index';
 
-bootstrap(LoginComponent);
+bootstrap(LoginComponent,[
+    HTTP_PROVIDERS,
+    { provide: XHRBackend, useClass: InMemoryBackendService }, // in-mem server
+    { provide: SEED_DATA, useClass: InMemoryDataService }      // in-mem server data
+]);
 
