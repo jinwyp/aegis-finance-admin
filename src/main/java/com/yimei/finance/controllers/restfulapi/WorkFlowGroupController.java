1491ab71b73cf4b26f42a1fa9a36e66d13135d88
liuxinjie@123 <liuxinjie@yimei180.com>
2016-08-10 17:48:01 +0800

    finance

--- src/main/java/com/yimei/finance/controllers/restfulapi/WorkFlowGroupController.java
+++ src/main/java/com/yimei/finance/controllers/restfulapi/WorkFlowGroupController.java
@@ -1,8 +1,9 @@
 package com.yimei.finance.controllers.restfulapi;
 
+import com.yimei.finance.repository.common.Result;
 import org.activiti.engine.IdentityService;
 import org.activiti.engine.ProcessEngine;
-import org.apache.ibatis.annotations.Result;
+import org.activiti.engine.identity.Group;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,17 +21,9 @@ public class WorkFlowGroupController {
 
     @RequestMapping("/group/add")
     public Result groupAddMethod(@RequestParam(value = "name", required = true)String name) {
-//        Group group = identityService.newGroup();
-//        group.setName(name);
-//        group.setType();
-//        //保存Group到数据库
-//        identityService.saveGroup(group);
-//        // 查询用户组
-//        Group data = identityService.createGroupQuery().groupId("1").singleResult();
-//        System.out.println(data.getId() + " --- " + data.getName() + " --- " + data.getType());
-//        // 设置名称
-//        data.setName("经理2组");
-//        identityService.saveGroup(data);
+        Group group = identityService.newGroup(String.valueOf(identityService.createGroupQuery().count() + 1));
+        group.setName(name);
+        identityService.saveGroup(group);
         return null;
     }
 
