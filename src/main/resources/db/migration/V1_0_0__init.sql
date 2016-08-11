769dd8d7b31a496b8491dc854481b185edce60e9
Zhangbolun <shijun@zrworldwide.com>
2016-08-11 09:07:15 +0800

    sql update

--- src/main/resources/db/migration/V1_0_0__init.sql
+++ src/main/resources/db/migration/V1_0_0__init.sql
@@ -31,6 +31,7 @@ CREATE TABLE `T_finance_apply_info_bills` (
   `id` int(11) NOT NULL,
   `T_finance_apply_info_id` int(11) DEFAULT NULL,
   `path` varchar(100) DEFAULT NULL COMMENT '路径',
+  `billtype` varchar(50) DEFAULT NULL COMMENT '类别',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT '申请信息账单表';
 
