539665728afc87fa23c2a39a17aff4fb37170d58
liuxinjie@123 <liuxinjie@yimei180.com>
2016-08-11 09:33:46 +0800

    finance

--- src/main/java/com/yimei/finance/controllers/restfulapi/finance/FinanceBasicController.java
+++ src/main/java/com/yimei/finance/controllers/restfulapi/finance/FinanceBasicController.java
@@ -1,7 +1,10 @@
 package com.yimei.finance.controllers.restfulapi.finance;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 处理交易员,业务员相关逻辑
  */
+@RestController
 public class FinanceBasicController {
 }
--- src/main/java/com/yimei/finance/controllers/restfulapi/finance/RiskManagementController.java
+++ src/main/java/com/yimei/finance/controllers/restfulapi/finance/RiskManagementController.java
@@ -1,7 +1,10 @@
 package com.yimei.finance.controllers.restfulapi.finance;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 处理风控相关逻辑
  */
+@RestController
 public class RiskManagementController {
 }
