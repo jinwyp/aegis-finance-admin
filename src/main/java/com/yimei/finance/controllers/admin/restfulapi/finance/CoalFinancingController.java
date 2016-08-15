bfaa65addbf0976b719e41e3af83e1316cbefe80
liuxinjie@123 <liuxinjie@yimei180.com>
2016-08-15 20:37:13 +0800

    finance

--- src/main/java/com/yimei/finance/controllers/admin/restfulapi/finance/CoalFinancingController.java
+++ src/main/java/com/yimei/finance/controllers/admin/restfulapi/finance/CoalFinancingController.java
@@ -4,6 +4,8 @@ import com.yimei.finance.repository.common.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.activiti.engine.RuntimeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,26 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * 处理煤易融相关逻辑
  */
-@RequestMapping("/api/financing")
-@Api(value = "CoalFinancingController", description = "煤易融相关方法")
+@RequestMapping("/api/finance/financing")
+@Api(value = "CoalFinancingApi", description = "煤易融相关接口")
 @RestController
 public class CoalFinancingController {
-//    @Autowired
-//    private EngineServices engineServices;
-//    @Autowired
-//    private RuntimeService runtimeService;
-//    @Autowired
-//    private IdentityService identityService;
-//    @Autowired
-//    private TaskService taskService;
+    @Autowired
+    private RuntimeService runtimeService;
 
     @RequestMapping(value = "/start", method = RequestMethod.POST)
-    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
-    @ApiImplicitParam(name = "id", value = "金融申请单id", required = true, dataType = "Long")
-    public Result startFinancingWorkFlow(@RequestParam(value = "id", required = true)int id) {
-//        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("financingWorkFlow");
-//        List<Task> taskList = taskService.createTaskQuery().
-//        taskService.addCandidateGroup(engineServices.getTaskService());
+    @ApiOperation(value = "发起流程", notes = "发起流程")
+    @ApiImplicitParam(name = "id", value = "金融申请单id", required = true, dataType = "String", paramType = "query")
+    public Object startFinancingWorkFlow(@RequestParam(value = "id", required = true) String id) {
+        runtimeService.startProcessInstanceByKey("financingWorkFlow", id);
         return Result.success();
     }
 
