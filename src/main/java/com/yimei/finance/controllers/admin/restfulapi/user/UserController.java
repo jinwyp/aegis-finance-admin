f7463fe9957afbf3cdc4a08aa808a2428ceb4cf9
liuxinjie@123 <liuxinjie@yimei180.com>
2016-08-16 11:16:52 +0800

    finance

--- src/main/java/com/yimei/finance/controllers/admin/restfulapi/user/UserController.java
+++ src/main/java/com/yimei/finance/controllers/admin/restfulapi/user/UserController.java
@@ -24,35 +24,27 @@ public class UserController {
     private IdentityService identityService;
 
     @RequestMapping(method = RequestMethod.POST)
-    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
-    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "UserEntity")
+    @ApiOperation(value = "创建用户", notes = "根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "UserEntity", paramType = "body")
     public Result addUserMethod(@RequestBody UserEntity user) {
         user.setId(null);
         identityService.saveUser(user);
         return Result.success().setData(user);
     }
 
-    /**
-     * 删除用户
-     * @param id                     用户 id
-     */
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     @ApiOperation(value = "删除用户", notes = "通过id删除用户")
     @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "String", paramType = "path")
-    public Result deleteUserMethod(@PathVariable("id")String id) {
+    public Result deleteUserMethod(@PathVariable("id") String id) {
         User user = identityService.createUserQuery().userId(id).singleResult();
         if (user == null) return Result.error(EnumUserError.此用户不存在.toString());
         identityService.deleteUser(id);
         return Result.success().setData(user);
     }
 
-    /**
-     * 修改用户
-     * @param user                   用户 对象
-     */
     @RequestMapping(method = RequestMethod.PUT)
-    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
-    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "UserEntity")
+    @ApiOperation(value = "创建用户", notes = "根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "UserEntity", paramType = "body")
     public Result updateUserMethod(@RequestBody UserEntity user) {
         if (user == null) return Result.error(EnumUserError.用户对象不能为空.toString());
         if (StringUtils.isEmpty(user.getId())) return Result.error(EnumUserError.用户id不能为空.toString());
@@ -64,12 +56,10 @@ public class UserController {
         return Result.success().setData(user);
     }
 
-    /**
-     * 根据 id 查询用户
-     * @param id                     用户 id
-     */
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Result getUserByIdMethod(@PathVariable("id")String id) {
+    @ApiOperation(value = "查询用户", notes = "根据id查询用户对象")
+    @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "String", paramType = "path")
+    public Result getUserByIdMethod(@PathVariable("id") String id) {
         User user = identityService.createUserQuery().userId(id).singleResult();
         if (user == null) return Result.error(EnumUserError.此用户不存在.toString());
         return Result.success().setData(user);
@@ -86,10 +76,11 @@ public class UserController {
 
     /**
      * 查询一个 用户 所在的 所有的组
-     * @param id                     用户 id
+     *
+     * @param id 用户 id
      */
     @RequestMapping(value = "/groups/{id}", method = RequestMethod.GET)
-    public Result getUserGroupsMethod(@PathVariable("id")String id, Page meta) {
+    public Result getUserGroupsMethod(@PathVariable("id") String id, Page meta) {
         meta.setTotal(identityService.createGroupQuery().groupMember(id).count());
         return Result.success().setData(identityService.createGroupQuery().groupMember(id).list()).setMeta(meta);
     }
