cdd8cfce706bdbfff2cce4161d57ba8bda382fc5
liuxinjie@123 <liuxinjie@yimei180.com>
2016-08-19 13:07:29 +0800

    finance

--- src/main/java/com/yimei/finance/controllers/admin/restfulapi/user/UserController.java
+++ src/main/java/com/yimei/finance/controllers/admin/restfulapi/user/UserController.java
@@ -1,6 +1,7 @@
 package com.yimei.finance.controllers.admin.restfulapi.user;
 
 import com.yimei.finance.entity.admin.user.EnumAdminUserError;
+import com.yimei.finance.entity.admin.user.GroupObject;
 import com.yimei.finance.entity.admin.user.UserObject;
 import com.yimei.finance.entity.common.result.Page;
 import com.yimei.finance.entity.common.result.Result;
@@ -14,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Api(tags = {"admin-api-user"})
 @RequestMapping("/api/financing/admin/users")
 @RestController
@@ -24,21 +27,16 @@ public class UserController {
     @Autowired
     private AdminUserServiceImpl userService;
 
-    @ApiOperation(value = "查询所有用户", notes = "查询所有用户列表", response = UserEntity.class, responseContainer = "List")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "page", value = "当前页数", required = false, dataType = "int", paramType = "query"),
-            @ApiImplicitParam(name = "count", value = "每页显示数量", required = false, dataType = "int", paramType = "query"),
-            @ApiImplicitParam(name = "offset", value = "偏移数", required = false, dataType = "int", paramType = "query"),
-            @ApiImplicitParam(name = "total", value = "结果总数量", required = false, dataType = "int", paramType = "query")
-    })
+    @ApiOperation(value = "查询所有用户", notes = "查询所有用户列表", response = UserObject.class, responseContainer = "List")
+    @ApiImplicitParam(name = "page", value = "当前页数", required = false, dataType = "int", paramType = "query")
     @RequestMapping(method = RequestMethod.GET)
     public Result getAllUsersMethod(Page page) {
         page.setTotal(identityService.createUserQuery().count());
-        return Result.success().setData(identityService.createUserQuery().listPage(page.getOffset(), page.getCount())).setMeta(page);
+        List<UserObject> userObjectList = DozerUtils.copy(identityService.createUserQuery().listPage(page.getOffset(), page.getCount()), UserObject.class);
+        return Result.success().setData(userObjectList).setMeta(page);
     }
 
-
-    @ApiOperation(value = "查询单个用户", notes = "根据id查询用户对象", response = UserEntity.class)
+    @ApiOperation(value = "查询单个用户", notes = "根据id查询用户对象", response = UserObject.class)
     @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "String", paramType = "path")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Result getUserByIdMethod(@PathVariable("id") String id) {
@@ -48,30 +46,26 @@ public class UserController {
         return Result.success().setData(userObject);
     }
 
-
-    @ApiOperation(value = "查询用户所在的组", notes = "查询某个用户所在的组")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "String", paramType = "path")
-    })
+    @ApiOperation(value = "查询用户所在的组", notes = "查询某个用户所在的组", response = GroupObject.class, responseContainer = "List")
+    @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "String", paramType = "path")
     @RequestMapping(value = "/{id}/groups", method = RequestMethod.GET)
     public Result getUserGroupsMethod(@PathVariable("id") String id, Page page) {
         page.setTotal(identityService.createGroupQuery().groupMember(id).count());
-        return Result.success().setData(identityService.createGroupQuery().groupMember(id).listPage(page.getOffset(), page.getCount())).setMeta(page);
+        List<GroupObject> groupObjectList = DozerUtils.copy(identityService.createGroupQuery().groupMember(id).list(), GroupObject.class);
+        return Result.success().setData(groupObjectList).setMeta(page);
     }
 
-
-
-    @ApiOperation(value = "创建用户", notes = "根据User对象创建用户", response = UserEntity.class)
+    @ApiOperation(value = "创建用户", notes = "根据User对象创建用户", response = UserObject.class)
     @RequestMapping(method = RequestMethod.POST)
-    public Result addUserMethod(@RequestBody UserEntity user) {
+    public Result addUserMethod(@ApiParam(name = "user", value = "用户对象", required = true)@RequestBody UserObject user) {
         if (StringUtils.isEmpty(user.getEmail())) return Result.error(EnumAdminUserError.用户登录名不能为空.toString());
-        if (identityService.createUserQuery().userEmail(user.getEmail()).singleResult() !=null) return Result.error(EnumAdminUserError.此登录名已经存在.toString());
+        if (identityService.createUserQuery().userEmail(user.getEmail()).singleResult() != null) return Result.error(EnumAdminUserError.此登录名已经存在.toString());
         user.setId(null);
         user.setPassword(userService.securePassword(user.getPassword()));
-        identityService.saveUser(user);
-        return Result.success().setData(identityService.createUserQuery().userId(user.getId()).singleResult());
+        UserEntity userEntity = DozerUtils.copy(user, UserEntity.class);
+        identityService.saveUser(userEntity);
+        return Result.success().setData(DozerUtils.copy(identityService.createUserQuery().userId(userEntity.getId()).singleResult(), UserObject.class));
     }
-
 
     @ApiOperation(value = "删除用户", notes = "根据 UserId 删除用户")
     @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "String", paramType = "path")
@@ -79,27 +73,26 @@ public class UserController {
     public Result deleteUserMethod(@PathVariable("id") String id) {
         User user = identityService.createUserQuery().userId(id).singleResult();
         if (user == null) return Result.error(EnumAdminUserError.此用户不存在.toString());
+        UserObject userObject = DozerUtils.copy(user, UserObject.class);
         identityService.deleteUser(id);
-        return Result.success().setData(user);
+        return Result.success().setData(userObject);
     }
 
 
-    @ApiOperation(value = "修改用户", notes = "根据 User Id修改用户")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "String", paramType = "path"),
-            @ApiImplicitParam(name = "email", value = "用户邮箱", required = true, dataType = "String", paramType = "form")
-    })
+    @ApiOperation(value = "修改用户", notes = "根据 User Id修改用户", response = UserObject.class)
+    @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "String", paramType = "path")
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public Result updateUserMethod(@PathVariable("id") String id, @RequestBody UserEntity user) {
+    public Result updateUserMethod(@PathVariable("id") String id,
+                                   @ApiParam(name = "user", value = "用户对象", required = true)@RequestBody UserObject user) {
         if (StringUtils.isEmpty(id)) return Result.error(EnumAdminUserError.用户id不能为空.toString());
         if (user == null) return Result.error(EnumAdminUserError.用户对象不能为空.toString());
-        User oldUser = identityService.createUserQuery().userId(user.getId()).singleResult();
+        User oldUser = identityService.createUserQuery().userId(id).singleResult();
         if (oldUser == null) return Result.error(EnumAdminUserError.此用户不存在.toString());
-        user.setPassword(oldUser.getPassword());
-        user.setId(id);
-        identityService.deleteUser(user.getId());
-        identityService.saveUser(user);
-        return Result.success().setData(identityService.createUserQuery().userId(user.getId()).singleResult());
+        oldUser.setFirstName(user.getFirstName());
+        oldUser.setLastName(user.getLastName());
+        oldUser.setEmail(user.getEmail());
+        identityService.saveUser(oldUser);
+        return Result.success().setData(DozerUtils.copy(identityService.createUserQuery().userId(id).singleResult(), UserObject.class));
     }
 
 
