85f8226294a7137e307208769dc43e70b977af33
qwang1990 <526186564@qq.com>
2016-08-19 10:09:55 +0800

    fix

--- src/main/java/com/yimei/finance/config/FreemarkerConfiguration.java
+++ src/main/java/com/yimei/finance/config/FreemarkerConfiguration.java
@@ -2,6 +2,7 @@ package com.yimei.finance.config;
 
 import freemarker.template.TemplateModelException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
@@ -22,11 +23,32 @@ public class FreemarkerConfiguration extends FreeMarkerAutoConfiguration.FreeMar
     @Autowired
     protected freemarker.template.Configuration configuration;
 
+    @Value("${ssourl.env}")
+    private String SSOURL;
+    @Value("${sso.protocol}")
+    private String SSOPROTOCOL;
+    @Value("${sitepage}")
+    private String SITEPAGE;
+    @Value("${logisticspage}")
+    private String LOGISTICSPAGE;
+    @Value("${pay.env}")
+    private String PAYURL;
+
+    @Value("${info.page}")
+    private String INFOPAGE;
+
     @PostConstruct
     void FreeMarkerConfigurer() throws TemplateModelException, IOException {
 
         configuration.setSharedVariable("staticPath","/static/site");
         configuration.setSharedVariable("title","易煤网");
+        configuration.setSharedVariable("ssoMemberUrl",SSOPROTOCOL+"://"+SSOURL);
+        configuration.setSharedVariable("payUrl",SSOPROTOCOL+"://"+PAYURL);
+        configuration.setSharedVariable("sitepage",SITEPAGE);
+        configuration.setSharedVariable("logisticspage",LOGISTICSPAGE);
+        configuration.setSharedVariable("infopage",INFOPAGE);
+
+
 
     }
 }
