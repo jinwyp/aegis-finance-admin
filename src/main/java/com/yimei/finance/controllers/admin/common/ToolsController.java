6b5e25973d59038b832ff8ea9e616e2ce429e039
liuxinjie@123 <liuxinjie@yimei180.com>
2016-08-23 10:09:35 +0800

    finance

--- src/main/java/com/yimei/finance/controllers/admin/common/ToolsController.java
+++ src/main/java/com/yimei/finance/controllers/admin/common/ToolsController.java
@@ -1,11 +1,14 @@
 package com.yimei.finance.controllers.admin.common;
 
+import com.yimei.finance.entity.admin.user.GroupObject;
 import com.yimei.finance.entity.common.databook.DataBook;
 import com.yimei.finance.entity.common.databook.EnumDataBookType;
 import com.yimei.finance.entity.common.result.Result;
 import com.yimei.finance.repository.admin.databook.DataBookRepository;
+import com.yimei.finance.utils.DozerUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.activiti.engine.IdentityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,10 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ToolsController {
     @Autowired
     private DataBookRepository dataBookRepository;
+    @Autowired
+    private IdentityService identityService;
 
     @RequestMapping(value = "/transportmodes", method = RequestMethod.GET)
     @ApiOperation(value = "获取运输方式列表", notes = "获取运输方式列表数据", response = DataBook.class, responseContainer = "List")
-    public Result findTransportModeList() {
+    public Result findTransportModeListMethod() {
         return Result.success().setData(dataBookRepository.findByType(EnumDataBookType.transportmode.toString()));
+    }
+
+    @RequestMapping(value = "/groups", method = RequestMethod.GET)
+    @ApiOperation(value = "获取所有组列表", notes = "获取所有用户组列表", response = GroupObject.class, responseContainer = "List")
+    public Result findAllUserGroupListMethod() {
+        return Result.success().setData(DozerUtils.copy(identityService.createGroupQuery().list(), GroupObject.class));
     }
 }
