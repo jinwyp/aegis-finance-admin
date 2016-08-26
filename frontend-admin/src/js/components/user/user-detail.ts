9eb227259b9708d0e7c2f3d2991d2477c1cf984a
liushengbin512 <liushengbin@zrworldwide.com>
2016-08-26 18:49:06 +0800

    提交基本信息修改

--- frontend-admin/src/js/components/user/user-detail.ts
+++ frontend-admin/src/js/components/user/user-detail.ts
@@ -4,6 +4,7 @@
 
 
 import { Component } from '@angular/core';
+import { User, UserService } from '../../service/user';
 
 
 declare var __moduleName: string;
@@ -13,13 +14,65 @@ declare var __moduleName: string;
     moduleId: __moduleName || module.id,
     templateUrl: 'user-detail.html'
 })
-
-
-
 export class UserDetailComponent {
 
-    title = '基本信息';
+    constructor(
+        private userService: UserService
+    ) {}
 
+    css = {
+        activeForRefresh : true,
+        isSubmitted :      false,
+        ajaxErrorHidden :  true
+    };
+
+    currentUserSession : User = new User();
+
+    departments  = [];
+
+    ngOnInit() {
+        this.getCurrentUser();
+    }
+
+    getCurrentUser() {
+
+        this.userService.getUserSessionObservable.subscribe(
+            result => {
+                if (result && result.success) {
+                    this.currentUserSession = result.data;
+                    console.log(this.currentUserSession)
+                } else {
+
+                }
+            },
+            error => console.error(error)
+        )
+    }
+
+    saveUser() {
+        this.css.ajaxErrorHidden = true;
+        this.css.isSubmitted     = true;
+
+        this.userService.save(this.currentUserSession).then((result)=> {
+            if (result.success) {
+                window.location.href = '/finance/admin/home/users';
+            } else {
+                this.css.ajaxErrorHidden = false;
+            }
+            console.log(result)
+        });
+    }
+
+    getDepartmentList() {
+        this.userService.getDepartmentList().then((result)=> {
+            if (result.success) {
+                this.departments = result.data;
+                console.log(result);
+            } else {
+
+            }
+        });
+    }
 
 }
 
