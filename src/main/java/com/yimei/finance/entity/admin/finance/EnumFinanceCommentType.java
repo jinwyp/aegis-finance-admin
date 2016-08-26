f6fc18c2e551ddcf0691498ed76a2ceaa7f7151d
liuxinjie@123 <liuxinjie@yimei180.com>
2016-08-27 00:09:14 +0800

    finance

--- src/main/java/com/yimei/finance/entity/admin/finance/EnumFinanceCommentType.java
+++ src/main/java/com/yimei/finance/entity/admin/finance/EnumFinanceCommentType.java
@@ -5,9 +5,9 @@ package com.yimei.finance.entity.admin.finance;
  */
 public enum EnumFinanceCommentType {
 
-    OnlineTraderAuditComment("GROUP00002_Audit", "线上交易员审核备注"),
-    OnlineTraderManageComment("GROUP0000", "线上交易员管理员备注"),
-    SalesmanAuditComment("12", "业务员审核备注"),
+    OnlineTraderAuditComment("AA_AA_AA_FLAG_AA_AA_AA_01", "线上交易员审核备注"),
+    OnlineTraderManageComment("AA_AA_AA_FLAG_AA_AA_AA_01", "线上交易员管理员备注"),
+    SalesmanAuditComment("AA_AA_AA_DD_FLAG_AA_01", "业务员审核备注"),
     SalesmanManageComment("9", "业务员管理员备注"),
     InvestigatorAuditComment("18", "尽调员审核备注"),
     InvestigatorManageComment("15", "尽调员管理员备注"),
@@ -23,12 +23,15 @@ public enum EnumFinanceCommentType {
     public String name;
 
     public static final String RiskManagerManageRight = "AA";
-    public static final String RiskManagerRight = "AA_BB";
-    public static final String InvestigatorManageRight = "AA_BB_FLAG_EE";
-    public static final String InvestigatorRight = "AA_BB_CCDEE_FF";
-    public static final String SupervisorManageRight = "AA_BB_CCDGG";
-    public static final String SupervisorRight = "AA_BB_CCDGG_HH";
-    public static final String SalesmanManageRight = "AA_BB_CCD_";
+    public static final String RiskManagerRight = "AA_AA";
+    public static final String InvestigatorManageRight = "AA_AA_AA_AA_FLAG";
+    public static final String InvestigatorRight = "AA_AA_AA_AA_FLAG_AA";
+    public static final String SupervisorManageRight = "AA_AA_AA_DD_FLAG";
+    public static final String SupervisorRight = "AA_AA_AA_DD_FLAG_AA";
+    public static final String SalesmanManageRight = "AA_AA_AA_FLAG_AA";
+    public static final String SalesmanRight = "AA_AA_AA_FLAG_AA_AA";
+    public static final String OnlineTraderManageRight = "AA_AA_AA_FLAG_AA_AA_AA";
+    public static final String OnlineTraderRight = "AA_AA_AA_FLAG_AA_AA_AA";
 
 
     EnumFinanceCommentType() {
