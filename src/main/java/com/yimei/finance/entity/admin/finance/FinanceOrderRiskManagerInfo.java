3f5c145f282da1b7ed483cdf2f45e32c8949143c
liuxinjie@123 <liuxinjie@yimei180.com>
2016-08-30 10:01:02 +0800

    finance

--- src/main/java/com/yimei/finance/entity/admin/finance/FinanceOrderRiskManagerInfo.java
+++ src/main/java/com/yimei/finance/entity/admin/finance/FinanceOrderRiskManagerInfo.java
@@ -9,36 +9,28 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
-@Table(name = "t_finance_order_supervisor_info")
+@Table(name = "t_finance_order_riskmanager_info")
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class FinanceOrderSupervisorInfo extends BaseEntity implements Serializable {
+public class FinanceOrderRiskManagerInfo extends BaseEntity implements Serializable {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;                                                 //主键
     @Column(name = "finance_id", nullable = false)
     private Long financeId;                                          //金融单id
-    @Column(name = "storage_place_name", length = 100)
-    private String storagePlaceName;                                 //仓储地名称
-    @Column(name = "storage_property", length = 100)
-    private String storageProperty;                                  //仓储性质
-    @Column(name = "storage_address", length = 200)
-    private String storageAddress;                                   //仓储地地址
-    @Column(name = "historical_cooperation_detail", length = 1000)
-    private String historicalCooperationDetail;                      //历史合作情况
-    @Column(name = "operating_storage_detail", length = 1000)
-    private String operatingStorageDetail;                           //经营及堆存情况
-    @Column(name = "port_standard_degree", length = 1000)
-    private String portStandardDegree;                               //保管及进出口流程规范程度
-    @Column(name = "supervision_cooperate_detail", length = 1000)
-    private String supervisionCooperateDetail;                       //监管配合情况
-    @Column(name = "supervision_scheme", length = 1000)
-    private String supervisionScheme;                                //监管方案
+    @Column(name = "distribution_ability_eval", length = 1000)
+    private String distributionAbilityEval;                          //分销能力评估
+    @Column(name = "payment_situation_eval", length = 1000)
+    private String paymentSituationEval;                             //预计回款情况
+    @Column(name = "business_risk_point", length = 1000)
+    private String businessRiskPoint;                                //业务风险点
+    @Column(name = "risk_control_scheme", length = 1000)
+    private String riskControlScheme;                                //风险控制方案
     @Column(name = "final_conclusion", length = 1000)
-    private String finalConclusion;                                  //最终结论/综合意见
+    private String finalConclusion;                                  //风控结论/最终结论/综合意见
     @Column(name = "need_supply_material", nullable = false)
     private boolean needSupplyMaterial;                              //需要补充材料 true: 需要, false: 不需要
     @Column(name = "supply_material_introduce", length = 500)
@@ -47,13 +39,13 @@ public class FinanceOrderSupervisorInfo extends BaseEntity implements Serializab
     private boolean noticeApplyUser;                                 //通知申请用户 true: 通知, false: 不通知
     @Column(name = "notice_salesman")
     private boolean noticeSalesman;                                  //通知业务员   true: 通知, false: 不通知
+    @Column(name = "edit_contract")
+    private boolean editContract;                                    //编辑和他     true: 需要编辑, false: 不需要编辑
     @Column(name = "audit_status_id", length = 3)
     private int auditStatusId;                                       //审核状态id
     @Column(name = "audit_status", length = 50)
     private String auditStatus;                                      //审核状态
     @Transient
     List<AttachmentObject> attachmentList;                           //附件列表
-
-
 
 }
--- src/main/java/com/yimei/finance/entity/common/basic/BaseEntity.java
+++ src/main/java/com/yimei/finance/entity/common/basic/BaseEntity.java
@@ -10,11 +10,11 @@ import java.util.Date;
 @MappedSuperclass
 @Data
 public class BaseEntity implements Serializable {
-    @Column(name = "create_man_id", nullable = false)
+    @Column(name = "create_man_id")
     private String createManId;                                      //创建人id
     @Column(name = "create_time")
     private Date createTime;                                         //创建时间
-    @Column(name = "last_update_man_id", nullable = false)
+    @Column(name = "last_update_man_id")
     private String lastUpdateManId;                                  //最后一次更新人id
     @Column(name = "last_update_time")
     private Date lastUpdateTime;                                     //最后一次更新时间
