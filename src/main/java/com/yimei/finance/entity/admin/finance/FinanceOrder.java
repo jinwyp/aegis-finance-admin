3ddee68c42cdb1cc84515c8056b762950d57a175
liuxinjie@123 <liuxinjie@yimei180.com>
2016-09-05 15:31:41 +0800

    finance

--- src/main/java/com/yimei/finance/entity/admin/finance/FinanceOrder.java
+++ src/main/java/com/yimei/finance/entity/admin/finance/FinanceOrder.java
@@ -34,15 +34,15 @@ public class FinanceOrder extends BaseEntity implements Serializable {
     private String applyType;                                        //申请类型(煤易融：MYR 煤易贷: MYD 煤易购: MYG)
     @Transient
     private String applyTypeName;
-    @Column(name = "financing_amount", length = 20)
+    @Column(name = "financing_amount", precision = 18, scale = 2)
     private BigDecimal financingAmount;                              //拟融资金额（单位：万元）
     @Column(name = "expect_date", length = 10, nullable = false)
     private int expectDate;                                          //拟使用资金时间（单位：天）
-    @Column(name = "business_amount", length = 10)
+    @Column(name = "business_amount", precision = 8, scale = 2)
     private BigDecimal businessAmount;                               //预期此笔业务量（单位：万吨）
     @Column(name = "transport_mode", length = 30)
     private String transportMode;                                    //运输方式：海运\汽运\火运\其他
-    @Column(name = "procurement_price", length = 10)
+    @Column(name = "procurement_price", precision = 8, scale = 2)
     private BigDecimal procurementPrice;                             //单吨采购价 (元/吨)
     @Column(name = "upstream_resource", length = 100)
     private String upstreamResource;                                 //上游资源方全称
@@ -56,13 +56,13 @@ public class FinanceOrder extends BaseEntity implements Serializable {
     private String downstreamContractor;                             //下游签约单位全称
     @Column(name = "terminal_server", length = 100)
     private String terminalServer;                                   //用煤终端
-    @Column(name = "selling_price", length = 10)
+    @Column(name = "selling_price", precision = 8, scale = 2)
     private BigDecimal sellingPrice;                                 //预计单吨销售价 (元/吨)
     @Column(name = "storage_location", length = 100)
     private String storageLocation;                                  //煤炭仓储地
     @Column(name = "coal_source", length = 100)
     private String coalSource;                                       //煤炭来源
-    @Column(name = "market_price", length = 10)
+    @Column(name = "market_price", precision = 8, scale = 2)
     private BigDecimal marketPrice;                                  //单吨市场报价（元／吨）
     @Column(name = "approve_state", length = 30, nullable = false)
     private String approveState;                                     //审批状态
