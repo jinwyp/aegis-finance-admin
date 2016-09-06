e67815fdf3ef13c1f7a4653f7ca0449e6ecd5d58
liuxinjie@123 <liuxinjie@yimei180.com>
2016-09-06 20:37:49 +0800

    finance

--- src/main/java/com/yimei/finance/entity/admin/finance/FinanceOrderSalesmanInfo.java
+++ src/main/java/com/yimei/finance/entity/admin/finance/FinanceOrderSalesmanInfo.java
@@ -20,17 +20,17 @@ public class FinanceOrderSalesmanInfo extends BaseEntity implements Serializable
     private Long id;                                                 //主键
     @Column(name = "finance_id", nullable = false, updatable = false, unique = true)
     private Long financeId;                                          //金融单id
-    @Column(name = "contract_companies_info_supply", length = 1000)
+    @Column(name = "contract_companies_info_supply", length = 1020)
     private String contractCompaniesInfoSupply;                      //上下游签约单位信息补充
-    @Column(name = "business_model_introduce", length = 1000)
+    @Column(name = "business_model_introduce", length = 1020)
     private String businessModelIntroduce;                           //业务操作模式介绍
-    @Column(name = "logistics_storage_info_supply", length = 1000)
+    @Column(name = "logistics_storage_info_supply", length = 1020)
     private String logisticsStorageInfoSupply;                       //物流仓储信息补充
-    @Column(name = "other_info_supply", length = 1000)
+    @Column(name = "other_info_supply", length = 1020)
     private String otherInfoSupply;                                  //其它补充说明
     @Column(name = "need_supply_material", length = 1, nullable = false)
     private int needSupplyMaterial;                                  //需要补充材料 1: 需要, 0: 不需要
-    @Column(name = "supply_material_introduce", length = 500)
+    @Column(name = "supply_material_introduce", length = 520)
     private String supplyMaterialIntroduce;                          //补充材料说明
     @Column(name = "notice_apply_user", length = 1, nullable = false)
     private int noticeApplyUser;                                     //通知申请用户 1: 通知, 0: 不通知
--- src/main/java/com/yimei/finance/representation/admin/finance/FinanceOrderSalesmanInfoObject.java
+++ src/main/java/com/yimei/finance/representation/admin/finance/FinanceOrderSalesmanInfoObject.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.List;
 
@@ -14,11 +15,20 @@ import java.util.List;
 public class FinanceOrderSalesmanInfoObject extends BaseEntity implements Serializable {
     private Long id;                                                 //主键
     private Long financeId;                                          //金融单id
+    @Size(max = 1000, message = "上下游签约单位补充信息不能超过 {max} 个字符")
     private String contractCompaniesInfoSupply;                      //上下游签约单位信息补充
+
+    @Size(max = 1000, message = "业务操作模式介绍不能超过 {max} 个字符")
     private String businessModelIntroduce;                           //业务操作模式介绍
+
+    @Size(max = 1000, message = "物流仓储补充信息不能超过 {max} 个字符")
     private String logisticsStorageInfoSupply;                       //物流仓储信息补充
+
+    @Size(max = 1000, message = "其它补充说明不能超过 {max} 个字符")
     private String otherInfoSupply;                                  //其它补充说明
     private int needSupplyMaterial;                                  //需要补充材料 1: 需要, 0: 不需要
+
+    @Size(max = 500, message = "补充材料说明不能超过 {max} 个字符")
     private String supplyMaterialIntroduce;                          //补充材料说明
     private int noticeApplyUser;                                     //通知申请用户 1: 通知, 0: 不通知
     private List<AttachmentObject> attachmentList;                   //附件列表
