8615a5223c2d84cd8cd492e25906fc161f798349
JinWYP <jinwyp@gmail.com>
2016-09-09 16:20:23 +0800

    fixed

--- frontend-admin/src/js/service/task.ts
+++ frontend-admin/src/js/service/task.ts
@@ -338,7 +338,7 @@ class TaskService {
 
                     if (!result.data){result.data = {}}
                     if (orderInfo.data) {
-                        result.data = Object.assign({}, result.data, orderInfo.data );
+                        result.data = Object.assign({}, orderInfo.data, result.data  );
                         // result.data.applyCompanyName          = orderInfo.data.applyCompanyName;
                         // result.data.ourContractCompany        = orderInfo.data.ourContractCompany;
                         // result.data.financingAmount           = orderInfo.data.financingAmount;
