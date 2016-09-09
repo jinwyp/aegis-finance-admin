251bdd4f53b6e2e2b37568c5db4f6e528ad59036
JinWYP <jinwyp@gmail.com>
2016-09-09 10:48:09 +0800

    update

--- frontend-admin/src/js/components/task/process-tab.ts
+++ frontend-admin/src/js/components/task/process-tab.ts
@@ -5,6 +5,8 @@
 
 import {Input, Component } from '@angular/core';
 
+import { Task, TaskService, TaskStatus } from '../../service/task';
+
 declare var __moduleName: string;
 
 @Component({
@@ -18,14 +20,46 @@ export class ProcessTabComponent {
         currentIndex : 2
     };
 
+
+    _financeId :number = -1;
+
+    get financeId() { return this._financeId; }
+
+    @Input()
+    set financeId(id: number) {
+
+        if (id && id > 0){
+            this._financeId = id;
+            this.getTaskList(this._financeId);
+        }
+    }
+
     @Input()
     processInstanceId : string = '0';
 
-    @Input()
     processList = [];
+
+
+    constructor(
+        private task: TaskService
+    ) {}
+
+
 
     changeTab = (currentTab)=>{
         this.css.currentIndex = currentTab;
+    };
+
+
+    getTaskList (financeId:number) {
+        this.task.getTaskListByOrderId(financeId).then((result)=>{
+            if (result.success && result.data){
+                this.processList = result.data;
+
+            }else{
+
+            }
+        });
     }
 }
 
