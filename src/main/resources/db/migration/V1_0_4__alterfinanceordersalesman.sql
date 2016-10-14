c5722582451d19feaed313e06c62bf903062dfc9
liuxinjie@123 <liuxinjie@yimei180.com>
2016-10-14 17:39:19 +0800

    finance

---
+++ src/main/resources/db/migration/V1_0_4__alterfinanceordersalesman.sql
@@ -0,0 +1,9 @@
+ALTER TABLE t_finance_order_salesman_info modify `contract_companies_info_supply` varchar(5020) DEFAULT NULL;
+
+ALTER TABLE t_finance_order_salesman_info modify `business_model_introduce` varchar(5020) DEFAULT NULL;
+
+ALTER TABLE t_finance_order_salesman_info modify `logistics_storage_info_supply` varchar(5020) DEFAULT NULL;
+
+ALTER TABLE t_finance_order_salesman_info modify `other_info_supply` varchar(5020) DEFAULT NULL;
+
+ALTER TABLE t_finance_order_salesman_info modify `supply_material_introduce` varchar(5020) DEFAULT NULL;
