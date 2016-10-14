c48e5a5df398b4a47a7b5254741cd59c90419a1d
liuxinjie@123 <liuxinjie@yimei180.com>
2016-10-14 18:01:30 +0800

    finance

---
+++ src/main/resources/db/migration/V1_0_6__alterfinanceordersupervisor.sql
@@ -0,0 +1,12 @@
+ALTER TABLE t_finance_order_supervisor_info modify `operating_storage_detail` TEXT DEFAULT NULL;
+
+ALTER TABLE t_finance_order_supervisor_info modify `port_standard_degree` TEXT DEFAULT NULL;
+
+ALTER TABLE t_finance_order_supervisor_info modify `supervision_cooperate_detail` TEXT DEFAULT NULL;
+
+ALTER TABLE t_finance_order_supervisor_info modify `final_conclusion` TEXT DEFAULT NULL;
+
+ALTER TABLE t_finance_order_supervisor_info modify `supply_material_introduce` TEXT DEFAULT NULL;
+
+ALTER TABLE t_finance_order_supervisor_info modify `supervision_scheme` TEXT DEFAULT NULL;
+
--- src/main/resources/db/migration/V1_0___alterfinanceordersupervisor.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE t_finance_order_supervisor_info modify `operating_storage_detail` TEXT DEFAULT NULL;
-
-ALTER TABLE t_finance_order_supervisor_info `port_standard_degree` TEXT DEFAULT NULL;
-
-ALTER TABLE t_finance_order_supervisor_info `supervision_cooperate_detail` TEXT DEFAULT NULL;
-
-ALTER TABLE t_finance_order_supervisor_info `final_conclusion` TEXT DEFAULT NULL;
-
-ALTER TABLE t_finance_order_supervisor_info `supply_material_introduce` TEXT DEFAULT NULL;
