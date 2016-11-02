94223e6c66ce01421b68be8ea9543e9bf9c8eba0
liuxinjie@123 <liuxinjie@yimei180.com>
2016-11-02 16:45:20 +0800

    user

--- src/main/java/com/yimei/finance/service/admin/company/AdminCompanyServiceImpl.java
+++ src/main/java/com/yimei/finance/service/admin/company/AdminCompanyServiceImpl.java
@@ -101,14 +101,14 @@ public class AdminCompanyServiceImpl {
      */
     public CompanyObject changeCompanyObject(Company company) {
         CompanyObject companyObject = DozerUtils.copy(company, CompanyObject.class);
-//        List<String> roleList = companyRoleRelationShipRepository.findRoleByCompanyId(company.getId());
-//        if (roleList != null && roleList.size() != 0) {
-//            final String[] roleName = {""};
-//            roleList.forEach(role -> {
-//                roleName[0] += EnumCompanyRole.valueOf(role) + " ";
-//            });
-//            companyObject.setRoleName(roleName[0]);
-//        }
+        List<String> roleList = companyRoleRelationShipRepository.findRoleByCompanyId(company.getId());
+        if (roleList != null && roleList.size() != 0) {
+            String roleName = "";
+            for (String role : roleList) {
+                roleName += EnumCompanyRole.valueOf(role) + " ";
+            }
+            companyObject.setRoleName(roleName);
+        }
         companyObject.setAdminName(userService.findCompanyFirstAdminName(company.getId()));
         return companyObject;
     }
