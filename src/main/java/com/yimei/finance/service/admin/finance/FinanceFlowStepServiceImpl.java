51e93caa6267b84d40c07dd34fda364dfa2afaa4
liuxinjie@123 <liuxinjie@yimei180.com>
2016-11-18 17:44:41 +0800

    task

--- src/main/java/com/yimei/finance/service/admin/finance/FinanceFlowStepServiceImpl.java
+++ src/main/java/com/yimei/finance/service/admin/finance/FinanceFlowStepServiceImpl.java
@@ -242,7 +242,7 @@ public class FinanceFlowStepServiceImpl {
         methodService.addAttachmentsMethod(riskManagerInfo.getAttachmentList4(), task.getId(), task.getProcessInstanceId(), EnumFinanceAttachment.Downstream_Contract_Attachment.toString());
         if (submit) {
             FinanceOrder financeOrder = financeOrderRepository.findOne(financeId);
-            if (taskMap.need == 0 && financeOrder.getApplyType().equals(EnumFinanceOrderType.MYD.toString())) {
+            if (taskMap.need == 0 && taskMap.pass == 1 && financeOrder.getApplyType().equals(EnumFinanceOrderType.MYD.toString())) {
                 FinanceOrderRiskManagerInfo financeOrderRiskManagerInfo = financeOrderRiskRepository.findByFinanceId(riskManagerInfo.getFinanceId());
                 if (financeOrderRiskManagerInfo == null)
                     return Result.error(EnumAdminFinanceError.你还没有提交合同信息.toString());
