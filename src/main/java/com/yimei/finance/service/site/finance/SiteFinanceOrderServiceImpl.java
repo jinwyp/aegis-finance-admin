3c859d18f2460c94aeed3ab0ce7ea0acbce6dfb6
liuxinjie@123 <liuxinjie@yimei180.com>
2016-11-18 17:41:00 +0800

    task

--- src/main/java/com/yimei/finance/service/site/finance/SiteFinanceOrderServiceImpl.java
+++ src/main/java/com/yimei/finance/service/site/finance/SiteFinanceOrderServiceImpl.java
@@ -53,6 +53,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SiteFinanceOrderServiceImpl {
@@ -181,18 +182,12 @@ public class SiteFinanceOrderServiceImpl {
     }
 
     public List<AttachmentObject> getAttachmentByFinanceIdType(Long financeId, EnumFinanceAttachment attachment, String type) {
-        List<AttachmentObject> attachmentObjectList = new ArrayList<>();
-        List<HistoricTaskInstance> taskList = historyService.createHistoricTaskInstanceQuery().processInstanceBusinessKey(String.valueOf(financeId)).taskDefinitionKey(attachment.type).list();
+        List<HistoricTaskInstance> taskList = historyService.createHistoricTaskInstanceQuery().processInstanceBusinessKey(String.valueOf(financeId)).taskDefinitionKey(attachment.type).orderByTaskCreateTime().desc().list();
         if (taskList == null || taskList.size() == 0) throw new BusinessException(EnumCommonError.Admin_System_Error);
-        taskList.parallelStream().forEach(task -> {
-            List<Attachment> attachments = taskService.getTaskAttachments(task.getId());
-            if (attachments != null && attachments.size() != 0) {
-                attachments.parallelStream().filter(a -> a.getType().equals(type)).forEach(a -> {
-                    attachmentObjectList.add(DozerUtils.copy(a, AttachmentObject.class));
-                });
-            }
-        });
-        return attachmentObjectList;
+        List<Attachment> attachmentList = taskService.getTaskAttachments(taskList.get(0).getId());
+        if (attachmentList == null || attachmentList.size() == 0) return null;
+        attachmentList = attachmentList.parallelStream().filter(a -> a.getType().equals(type)).collect(Collectors.toList());
+        return DozerUtils.copy(attachmentList, AttachmentObject.class);
     }
 
     /**
